80d7ee02edb506982915e314e7c0129f87d5f891
Amine <mouhamedaymenhammami@gmail.com>
2020-01-18 23:25:48 +0100

    materialUI ssr and body bgcolor

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,55 @@
-import Document, { DocumentContext } from "next/document";
+import React from "react";
+import Document, {
+  DocumentContext,
+  Head,
+  Main,
+  NextScript
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import theme from "../theme";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
+    const styledComponentsSheets = new ServerStyleSheet();
+    const materialUISheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: App => props => {
+            return materialUISheets.collect(
+              styledComponentsSheets.collectStyles(<App {...props} />)
+            );
+          }
         });
 
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
+        styles: [
+          ...React.Children.toArray(initialProps.styles),
+          styledComponentsSheets.getStyleElement(),
+          materialUISheets.getStyleElement()
+        ]
       };
     } finally {
-      sheet.seal();
+      styledComponentsSheets.seal();
     }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+        </Head>
+        <body style={{ backgroundColor: "#ebeef2" }}>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }

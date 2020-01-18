9b7c95b588bdcf7076c484124238142ee2da87d1
Amine <mouhamedaymenhammami@gmail.com>
2020-01-18 21:58:21 +0100

    add storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../components", true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  backgrounds: [
+    { name: "twitter", value: "#00aced", default: true },
+    { name: "facebook", value: "#3b5998" }
+  ]
+});
 
 configure(loadStories, module);

50265f9311af7c19d99c32d47304dfd972d86d51
Amine <mouhamedaymenhammami@gmail.com>
2020-01-18 23:55:31 +0100

    move mockeddata outside index

---
+++ mockedData.ts
@@ -0,0 +1,26 @@
+import { name, internet, random } from "faker";
+
+const itemsArray = new Array(
+  random.number({
+    min: 5,
+    max: 15
+  })
+).fill(true);
+
+const generateTags = () =>
+  new Array(
+    random.number({
+      min: 1,
+      max: 5
+    })
+  )
+    .fill(true)
+    .map(_ => random.word());
+
+export const bookmarks = itemsArray.map(_ => {
+  return {
+    title: name.title(),
+    url: internet.url(),
+    tags: generateTags()
+  };
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,15 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
-import { name, internet, random } from "faker";
 import Grid, { GridSpacing } from "@material-ui/core/Grid";
 import BookmarkCard from "../components/bookmark-card/bookmark-card";
-
-const itemsArray = new Array(
-  random.number({
-    min: 5,
-    max: 15
-  })
-).fill(true);
-
-const generateTags = () =>
-  new Array(
-    random.number({
-      min: 1,
-      max: 5
-    })
-  )
-    .fill(true)
-    .map(_ => random.word());
-
-const mockData = itemsArray.map(_ => {
-  return {
-    title: name.title(),
-    url: internet.url(),
-    tags: generateTags()
-  };
-});
+import { bookmarks } from "../mockedData";
 
 const Home = () => (
   <Container>
     <br />
     <br />
     <Grid container spacing={4}>
-      {mockData.map((b, index) => (
+      {bookmarks.map((b, index) => (
         <Grid item key={index}>
           <BookmarkCard
             tags={b.tags}

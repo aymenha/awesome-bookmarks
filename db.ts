431562fd766538cd53744548a83b2dbe792a4969
Amine <mouhamedaymenhammami@gmail.com>
2020-02-02 19:32:46 +0100

    setup pouchdb connection

---
+++ db.ts
@@ -0,0 +1,30 @@
+import PouchDB from "pouchdb";
+import PouchDBFind from "pouchdb-find";
+
+let _db: PouchDB.Database | null;
+
+const init = () => {
+  try {
+    PouchDB.plugin(PouchDBFind);
+    PouchDB.sync("bookmarks", "http://localhost:5984/bookmarks", {
+      live: true
+    });
+    console.log("db initialized");
+    _db = new PouchDB("bookmarks");
+    return _db;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getInstance = () => {
+  if (_db) return _db;
+  return init();
+};
+
+export const resetDb = async () => {
+  if (!_db) return init();
+  const docs = await _db.allDocs({ include_docs: true });
+  docs.rows.filter(row => row.doc).map(row => _db!.remove(row.doc!));
+};
